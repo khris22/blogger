d6b451d16f74362204e5e8765793c8a9e5e01e8d
khris22 <khrispunzalan@gmail.com>
2020-06-04 12:11:22 -0700

    added current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+    include ::ActionController::Cookies
+
+    def current_user
+        # finding the current_user
+        User.find_by(id: session[:user_id])
+    end
+    
+    def logged_in?
+        !!current_user
+    end
+
 end
