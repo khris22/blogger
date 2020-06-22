ac5587651a7631754b9b904c95f8fb628be94ffb
khris22 <khrispunzalan@gmail.com>
2020-06-22 12:09:09 -0400

    added create method for Entry

--- app/controllers/api/v1/entries_controller.rb
+++ app/controllers/api/v1/entries_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::EntriesController < ApplicationController
 
+    before_action :set_trip, only: [:show, :update, :destroy]
+
+
     def index
         # binding.pry
         if logged_in?
@@ -11,4 +14,41 @@ class Api::V1::EntriesController < ApplicationController
               }
         end
     end
+
+    def show
+        render json: @entry
+    end
+
+    def create
+        # byebug
+        @entry = current_user.entries.build(entry_params)
+    
+        if @entry.save
+          render json:  EntrySerializer.new(@entry), status: :created
+        else
+          error_resp = {
+            error: @entry.errors.full_messages.to_sentence
+          }
+          render json: error_resp, status: :unprocessable_entity
+        end
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def set_entry
+        @entry = Entry.find(params[:id])
+    end
+
+    def entry_params
+        params.require(:entry).permit(:title, :notes)
+        # params.require(:entry).permit(:title, :notes, :user_id)
+        # :user_id - when you want to pass it without using current_user.build
+    end
+
 end
